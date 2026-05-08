99c06cdf9309dbb4d09025f80bf18a6c5d927f37
Wes Bezzant <wes.bezzant@gmail.com>
2026-05-07 23:57:09 -0600

    vite updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   // IMPORTANT: This must be your GitHub repository name
   // Example: if your URL is github.com/wbezzant/crossword-mvp, use '/crossword-mvp/'
-  base: '/crossword-mvp/', 
+  base: '/crossword-mvp/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        game: resolve(__dirname, 'game.html'),
+      },
+    },
+  },        
 });
