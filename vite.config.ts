5bc221cddf29af1dac259f4cdd090fe5eba4b9be
Wes Bezzant <wes.bezzant@gmail.com>
2026-05-07 23:20:43 -0600

    Added yml and vite config

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // IMPORTANT: This must be your GitHub repository name
+  // Example: if your URL is github.com/wbezzant/crossword-mvp, use '/crossword-mvp/'
+  base: '/crossword-mvp/', 
+});
